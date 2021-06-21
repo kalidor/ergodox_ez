4976dd90762da8c2ac4c90080faf465aaa6f9e08
kalidor <kalidor@unixed.fr>
2021-06-21 13:56:25 +0200

    Update volume and fix command line (ubuntu)

--- ergodox_build.sh
+++ ergodox_build.sh
@@ -2,7 +2,7 @@
 make ergodox_ez:kalidor
 echo "Push reset button on Ergodox-EZ keyboard"
 sleep 3
-teensy-loader-cli -v -mmcu=atmega32u4 ergodox_ez_kalidor.hex
+teensy_loader_cli -v -mmcu=atmega32u4 ergodox_ez_kalidor.hex
 
 echo ""
 echo "Remember to setxfrmap us intel to get azerty keyoard. Yeah, I know..."
